519f1f98d4be23511e55a061dec253b4d35bf246
Conor Callahan <conorcallahan97@outlook.com>
2024-06-24 19:19:13 -0500

    Finished the front end Post for the router.

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -5,8 +5,22 @@ const pool = require('../modules/pool')
 
 // TODO: This route adds a new feedback entry
 router.post('/', (req, res) => {
+    const feedback = req.body;
+    console.log(feedback, 'feedback');
+    const sqlText = `INSERT INTO feedback 
+    ("feeling", "understanding", "support", "comments")
+    VALUES ($1, $2, $3, $4)`;
 
-})
+    pool.query(sqlText, [feedback.feeling, feedback.understanding, feedback.support, feedback.comments])
+    .then((result) => {
+        console.log(`Added new feedback to the database`, feedback);
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+    });
+});
 
 
 // DO NOT EDIT THIS ROUTE
