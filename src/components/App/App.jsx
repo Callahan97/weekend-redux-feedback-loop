aee0f5174282da37d88390c595aac53dde1eb288
Conor Callahan <conorcallahan97@outlook.com>
2024-06-24 19:31:00 -0500

    Added the starting components and their folders, am gonna wait to see how this will work with the redux additions so im gonna hold back for now. Began my additons to the App.jsx file to create the needed pathing.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,24 @@
+import {useEffect, useState} from 'react';
 import React from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const fetchFeedback = () => {
+    axios({
+      method: 'GET',
+      url: 'api/feedback'
+    }).then((response) => {
+      console.log(response);
+      console.log(response.data);
+      setFeedbackList(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  const [feedbackList, setFeedbackList] = useState([]);
 
   return (
     <div className='App'>
