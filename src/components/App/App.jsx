82250b0175cd2ceab40e3d56f5017b96077f2775
Conor Callahan <conorcallahan97@outlook.com>
2024-07-08 23:19:16 -0500

    Support comp is completed and i got rid of a useless GET that i had to test it to work and cleaned up the app.jsx. Im planning on finishing the understanding comp to add them to the app to clarify and rectify any issues with importing them into the App.jsx.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,30 +3,11 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+
+
 function App() {
 
-  const fetchFeedback = () => {
-    axios({
-      method: 'GET',
-      url: 'api/feedback'
-    }).then((response) => {
-      console.log(response);
-      console.log(response.data);
-      setFeedbackList(response.data);
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
-
-  const [feedbackList, setFeedbackList] = useState([]);
-
-  useEffect(fetchFeedback, []);
-
- 
-
-
   
-
  
 
   return (
---
+++ src/components/Supported/supported.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+
+const SupportComp = () => {
+    const [support, setSupport] = useState('');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+
+    const handleNext = () => {
+        dispatch({ type: 'SET_SUPPORT', payload: support });
+        navigate('/comments');
+    };
+
+
+    return (
+        <div>
+            <h2>How well are you being supported?</h2>
+            <input
+                type="number"
+                value={support}
+                onChange={(e) => setSupport(e.target.value)}
+                data-testid="input"
+            />
+            <button onClick={handleNext} data-testid="next">Next</button>
+        </div>
+    );
+};
+
+export default SupportComp;
