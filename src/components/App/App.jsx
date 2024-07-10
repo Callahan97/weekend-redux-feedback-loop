9be62f50597b1e321c29ca3dc2621381eb384e32
Conor Callahan <conorcallahan97@outlook.com>
2024-07-10 01:10:59 -0500

    After completeing the understanding portion of the form i moved on to my previously bare app.jsx and imported all the required components and hooks like use navigate. Unfortunately after fixing my pathing for them after a couple hours i immediately ran into issues after completeing the app.jsx to a good degree. I began having issues with useNavigate itself.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,52 @@
 import {useEffect, useState} from 'react';
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import store from '../../store';
 import './App.css';
 
+import FeelingComp from '../Feeling/feeling';
+import UnderstandingComp from '../Understanding/understanding';
+import SupportComp from '../Supported/supported';
+import { Provider } from 'react-redux';
 
 
 function App() {
+  const [feedbackList, setFeedbackList] = useState([]);
+  
+
+  const fetchFeedback = () => {
+    axios({
+      method: 'GET',
+      url: 'api/feedback'
+    }).then((response) => {
+      console.log(response);
+      console.log(response.data);
+      setFeedbackList(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  useEffect(fetchFeedback, []);
 
   
- 
-
   return (
+    <Provider store={store}>
+    <Router>
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Routes>
+        <Route path="/" element={FeelingComp} />
+        <Route path="/understanding" element={UnderstandingComp} />
+        <Route path="/support" element={SupportComp} />
+      </Routes>
     </div>
+    </Router>
+    </Provider>
   );
 }
 
---
+++ src/components/Understanding/understanding.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const UnderstandingComp = () => {
+    const [understanding, setUnderstanding] = useState('');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+
+    const handleNext = () => {
+        dispatch({ type: 'SET_UNDERSTANDING', payload: understanding });
+        navigate('/support');
+    };
+
+    return (
+        <div>
+            <h2>How well are you understanding the content?</h2>
+
+            <input type="number"
+                value={understanding}
+                onChange={(e) => setUnderstanding(e.target.value)}
+                data-testid="input"/>
+                
+            <button onClick={handleNext} data-testid="next">Next</button>
+        </div>
+    );
+};
+
+export default UnderstandingComp;
